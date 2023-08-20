acf3aca7152a311335042474ea6a6577757fc2bc
tahmidu <tahmid.uddin.dev@gmail.com>
2023-08-20 18:53:29 +0100

    created create_message_table migration and complete

---
+++ database/migrations/2023_08_20_175059_create_message_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('message');
+            $table->boolean('read');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('chat_id')->references('id')->on('chat')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('message');
+    }
+};

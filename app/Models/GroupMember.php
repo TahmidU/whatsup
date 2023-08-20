cebb13900af066e4450399230e1e9f77a1cf1552
tahmidu <tahmid.uddin.dev@gmail.com>
2023-08-20 18:27:43 +0100

    Model relationship defined

--- app/Models/GroupMember.php
+++ app/Models/GroupMember.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class GroupMember extends Model
 {
     use HasFactory;
+
+    public function Group(): BelongsTo{
+        return $this->belongsTo(Group::class);
+    }
+
+    public function User(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }

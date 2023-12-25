f248d4529670b0a8982619ae3864e25eb3874a46
tahmidu <tahmid.uddin.dev@gmail.com>
2023-12-25 14:12:42 +0000

    TimerButton story fix

--- resources/js/Components/Atoms/Buttons/TimerButton/TimerButton.stories.tsx
+++ resources/js/Components/Atoms/Buttons/TimerButton/TimerButton.stories.tsx
@@ -5,21 +5,35 @@ import { ComponentProps } from "react";
 const payload: MetaPayload<typeof TimerButton> = {
     title: "ATOMS/Buttons/TimerButton",
     component: TimerButton,
+    parameters: {
+        controls: {
+            exclude: ["onTimerEnd", "timerKey"],
+        },
+    },
     argTypes: {
         animState: {
-            options: ["paused", "running"],
-            control: {
-                type: "radio",
-            },
+            name: "animation state",
         },
     },
 };
 export default payload;
 
-export const Basic = (args: ComponentProps<typeof TimerButton>) => (
-    <TimerButton {...args}>Some content</TimerButton>
-);
+export const Basic = (args: ComponentProps<typeof TimerButton>) => {
+    const timerKey = args.animState === "stop" ? "reset" : "persist";
+
+    return (
+        <TimerButton
+            {...args}
+            timerKey={timerKey}
+            style={{ width: "fit-content" }}
+        >
+            <span style={{ margin: "0.5rem 1rem 1rem 0.5rem" }}>
+                Any content can go here
+            </span>
+        </TimerButton>
+    );
+};
 Basic.args = {
-    animState: "paused",
+    animState: "play",
     time: 10,
 };

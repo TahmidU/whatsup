f59b20b749f68e5d0ed1fe4af1596446e7350361
tahmidu <tahmid.uddin.dev@gmail.com>
2024-01-06 12:47:22 +0000

    WorldMap story cleaned

--- resources/js/Components/Organisms/Home/WorldMap/WorldMap.stories.tsx
+++ resources/js/Components/Organisms/Home/WorldMap/WorldMap.stories.tsx
@@ -5,10 +5,21 @@ import { ComponentProps } from "react";
 const payload: MetaPayload<typeof WorldMap> = {
     title: "ORGANISMS/WorldMap",
     component: WorldMap,
+    argTypes: {
+        selectionInterval: {
+            name: "animation time (seconds)",
+        },
+        selectNum: {
+            name: "number of points",
+        },
+    },
 };
 export default payload;
 
 export const Basic = (args: ComponentProps<typeof WorldMap>) => (
     <WorldMap {...args} />
 );
-Basic.args = {};
+Basic.args = {
+    selectionInterval: 2.5,
+    selectNum: 50,
+};
--- resources/js/Components/Organisms/Home/WorldMap/WorldMap.tsx
+++ resources/js/Components/Organisms/Home/WorldMap/WorldMap.tsx
@@ -14,6 +14,7 @@ export default function WorldMap({
 
     return (
         <SVGContainer
+            key={`${selectNum}${selectionInterval}`}
             $selectionInterval={selectionInterval}
             xmlns="http://www.w3.org/2000/svg"
             version="1.1"

d2dc8ebb70b09e4cf60355b9b1996b98a286c727
tahmidu <tahmid.uddin.dev@gmail.com>
2024-03-09 22:08:00 +0000

    Tailwind installed and configured

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,6 +2,7 @@ import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import "react-toastify/dist/ReactToastify.css";
 import AppSetup from "@/configs/AppSetup";
+import "../css/app.css";
 
 createInertiaApp({
     resolve: (name) => {
